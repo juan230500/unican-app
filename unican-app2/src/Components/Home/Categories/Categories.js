3f74f40bed43d3c86975a163f9ebac2c82e91a6b
juan <juanpo.alvarado@gmail.com>
2021-06-07 21:25:23 -0600

    [ADD] Carousel

--- unican-app2/src/Components/Home/Categories/Categories.js
+++ unican-app2/src/Components/Home/Categories/Categories.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import classes from "./Categories.module.css";
 import imgCubeta from "../../../assets/Cubeta.jpg";
 import imgBidon from "../../../assets/Bidon.jpg";
@@ -6,13 +8,25 @@ import imgCanasta from "../../../assets/Canasta.jpg";
 
 import { useHistory } from "react-router-dom";
 import Category from "./Category/Category";
+import Slider from "react-slick";
 
 const Categories = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 1000,
+    autoplay: true,
+    draggable: true,
+    pauseOnHover: true,
+  };
   const history = useHistory();
   return (
     <div className={classes.Container}>
       <h1 className={classes.Title}>Nuestros productos</h1>
-      <div className={classes.Row}>
+      <Slider {...settings} arrows>
         <Category
           onClick={() => history.push("/catalog?Canastas")}
           title="Canastas"
@@ -28,7 +42,22 @@ const Categories = () => {
           title="Cubetas"
           img={imgCubeta}
         ></Category>
-      </div>
+        <Category
+          onClick={() => history.push("/catalog?Canastas")}
+          title="Canastas"
+          img={imgCanasta}
+        ></Category>
+        <Category
+          onClick={() => history.push("/catalog?Bidones")}
+          title="Bidones"
+          img={imgBidon}
+        ></Category>
+        <Category
+          onClick={() => history.push("/catalog?Cubetas")}
+          title="Cubetas"
+          img={imgCubeta}
+        ></Category>
+      </Slider>
     </div>
   );
 };
